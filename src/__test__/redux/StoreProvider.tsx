d57670a0a94046eb8c3e2f0197c28468f7978467
Joven Luma-as <jovenlumaas@gmail.com>
2025-04-04 11:16:48 +0800

    update test

--- src/__test__/redux/StoreProvider.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import React from 'react';
-import { Provider as ReduxProvider } from 'react-redux';
-import store from './store';
-
-export default function StoreProvider({ children }: { children: React.ReactNode }) {
-  return <ReduxProvider store={store}>{children}</ReduxProvider>;
-}
